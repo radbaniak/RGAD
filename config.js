bde24c2a16b198f07ad7e58385e25e428ffc5836
Radoslaw Baniak <radoslawbaniak@googlemail.com>
2022-08-06 15:22:47 +0100

    public

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/favicon.png',
+  manifestIcon: 'src/assets/images/rga-logo-cropped.png',
   pathPrefix: `/RGAD`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Roger Gracie Academy',
   subHeading: 'Brazilian Jiu-Jitsu Academy, Darlington.  ',
