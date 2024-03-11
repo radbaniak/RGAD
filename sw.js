bf867459134d6f7ff139cc64eddc8b66aa8b5f21
Radoslaw Baniak <radoslawbaniak@googlemail.com>
2024-03-11 15:55:45 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-09a2b9ae58f4b4d4e4f9.js"
+    "url": "webpack-runtime-9d042e0ca7a44ce757b0.js"
   },
   {
     "url": "framework-8e0da09bb1d34e5785e9.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a2aac7bf17e388bed19d191995c65f39"
+    "revision": "8cfbbab51bf82c051e260801849dc9f3"
   },
   {
     "url": "polyfill-48d0fb1ece1335f5f663.js"
